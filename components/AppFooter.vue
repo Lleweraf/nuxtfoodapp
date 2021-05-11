1da6df706b73c21de834153fdced57d6d0fa41d3
Ralph Guintu <rlguintu@gmail.com>
2021-05-11 08:35:00 +0800

    initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>© {{ getYear() }} All rights reserved.</footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      year: '',
+    }
+  },
+  methods: {
+    getYear() {
+      const d = new Date()
+      return (this.year = d.getFullYear())
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
